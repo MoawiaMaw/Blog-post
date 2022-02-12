ff6bfeb7602c48d84fe7822390a598402e61b874
MoawiaMaw <agwaenanln@gmail.com>
2022-02-13 00:10:06 +0300

    added config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const con = await mongoose.connect('mongodb://localhost/ca-blogpost-app', {
+    const con = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
     });
     console.log(`mongoDB connected: ${con.connection.host}`);
